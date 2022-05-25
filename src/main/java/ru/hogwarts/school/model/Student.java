df96b1d11c53e4d1a18afef698c5aaf4fac26020
Micelovecat <flora_bux@mail.ru>
2022-05-25 19:14:53 +0300

    First commit 3HomeWork4

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -26,23 +26,18 @@ public class Student {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
